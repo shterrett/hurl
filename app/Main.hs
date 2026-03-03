342aa41b75cc33a3d786f3a38bd1b96bc33a1409
Stuart Terrett <shterrett@gmail.com>
2026-03-02 20:50:15 -0500

    Main runs the program

--- app/Main.hs
+++ app/Main.hs
@@ -2,14 +2,17 @@
 
 module Main where
 
+import qualified Data.ByteString.Char8 as BSC
+import qualified Data.ByteString.Lazy as LBS
 import Data.Text (Text)
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
 import Error.Diagnose
 import Error.Diagnose.Compat.Megaparsec
-import Hurl (parseRequest)
+import Hurl (HurlResponse(..), parseRequest, sendRequest)
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
+import System.IO (hPutStrLn)
 import Text.Megaparsec (runParser)
 
 main :: IO ()
@@ -19,7 +22,13 @@ main = do
     [filename] -> do
       content <- Text.readFile filename
       case runParser parseRequest filename content of
-        Right req -> print req
+        Right req -> do
+          result <- sendRequest req
+          case result of
+            Left httpErr -> do
+              hPutStrLn stderr ("HTTP error: " <> show httpErr)
+              exitFailure
+            Right resp -> printResponse resp
         Left bundle -> do
           let diag  = errorDiagnosticFromBundle
                         (Nothing :: Maybe Text)
@@ -30,3 +39,12 @@ main = do
           printDiagnostic stderr WithUnicode (TabSize 4) defaultStyle diag'
           exitFailure
     _ -> putStrLn "Usage: hurl-exe <filename.hurl>" >> exitFailure
+
+printResponse :: HurlResponse -> IO ()
+printResponse resp = do
+  putStrLn ("HTTP " <> show (hurlResponseStatus resp))
+  mapM_ printHeader (hurlResponseHeaders resp)
+  putStrLn ""
+  LBS.putStr (hurlResponseBody resp)
+  where
+    printHeader (name, value) = BSC.putStrLn (name <> ": " <> value)
